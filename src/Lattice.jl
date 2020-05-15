57ce7ddb80811e9b12ae87020c64fbc283a4cffc
fengshi96 <fengshichn@gmail.com>
2020-05-15 10:19:16 -0400

    update May 15

--- src/Lattice.jl
+++ src/Lattice.jl
@@ -1,56 +1,52 @@
-using SparseArrays
 using LinearAlgebra
-using Arpack
 
-module  Lattice
+function Honeycomb(LLX, LLY, BC::String = "OBC")
 
-	LLX = 2; LLY = 2
+	#LLX = 2; LLY = 2
 	Nsite = LLX * LLY * 2
 	number1neigh = 3
 	println("creating Honeycomb lattice... ")
 
-	scalex = 2; scaley = 4.0/sqrt(3)
+	scalex::Float64 = 2; scaley::Float64 = 4.0/sqrt(3)
 	t1 = [1.0 * scalex; 0]
 	t2 = [0.5 * scalex; sqrt(3)/2.0 * scaley]
 
 	indx_ = Vector{Int8}(undef, Nsite)  # x coordinate of sites
 	indy_ = Vector{Int8}(undef, Nsite)  # y coordinate of sites
-	mesh_ = Matrix{Int8}(undef, LLX*2+LLY,LLY*2)  # lattice on meshgrid
+	mesh_ = fill!(Matrix{Int8}(undef, LLX*2+LLY,LLY*2),0)  # lattice on meshgrid
 	nn_ = Matrix{Int8}(undef, Nsite, number1neigh)  # nearest neighbors
 	
 	# ---------------- Construct Lattice mesh ------------------------ 
 	# ---------------- Construct Lattice mesh ------------------------ 
 	# ---------------- Construct Lattice mesh ------------------------ 
-	xv = 1  # xv is unit cell length in x (vertival) direction
-	counter = 1	
+	xv::Int8 = 1  # xv is unit cell length in x (vertival) direction
+	counter::Int8 = 1	
 	for i in 1: LLX
 		if i!=1
-			global xv += t1[1]
+			xv += t1[1]
 		end
 				
-		xa = xv; xb = xv + 1
-		ya = 1; yb = 2
+		xa::Int8 = xv; xb::Int8 = xv + 1
+		ya::Int8 = 1; yb::Int8 = 2
 		
 		for j in 1:LLY
 			indx_[counter] = xa
 			indy_[counter] = ya
-			mesh_[Int8(xa),Int8(ya)] = counter
+			mesh_[xa,ya] = counter
 			#println("xa=", xa, " ya=",ya, " counter=",counter)
-			global counter += 1
+			counter += 1
 			
 			indx_[counter] = xb
 			indy_[counter] = yb
-			mesh_[Int8(xb),Int8(yb)] = counter
+			mesh_[xb,yb] = counter
 			#println("xb=", xb, " yb=",yb, " counter=",counter)
-			global counter += 1
+			counter += 1
 			
 			xa = xv + j*t2[1]; xb = (xv + 1) + j*t2[1]
 			ya = 1 + j*t2[2]; yb = 2 + j*t2[2]
 										
 		end		
 	end
-	show(stdout, "text/plain", mesh_)
-	println()
 
 	# ------------ Construct matrix of 1st n.n.s --------------------- 
 	# ------------ Construct matrix of 1st n.n.s --------------------- 
@@ -60,12 +56,12 @@ module  Lattice
 	ymax = findmax(indy_)[1]
 	
 	for i in 1:Nsite
-		ix = indx_[i]  # coordinate of n-th site in matrix
-		iy = indy_[i]
+		ix::Int8 = indx_[i]  # coordinate of n-th site in matrix
+		iy::Int8 = indy_[i]
 				
 		#----------------------------OBC-----------------------------------
 		#  n.n in x-bond
-		jx = ix + 1; jy = iy + 1  # move 1 step forward in x = (1,1) direction
+		jx::Int8 = ix + 1; jy::Int8 = iy + 1  # move 1 step forward in x = (1,1) direction
 		if jx <= xmax && jy <= ymax && mesh_[jx,jy] != 0
 			j = mesh_[jx,jy]  # site index of n.n. in x direction
 			nn_[i,1] = j
@@ -91,40 +87,33 @@ module  Lattice
 		
 		
 		#--------------------------Apply PBC-------------------------------	
-		# z-bond
-		jx = ix - LLY 
-		jy = 1
-		if jx >= 1 && iy == ymax && mesh_[jx,jy]!=0
-			j = mesh_[jx,jy]
-			nn_[i,3] = j
-			nn_[j,3] = i
-		end
-		
-		# y-bond
-		jx = ix + 2 * LLX - 1 
-		jy = iy + 1
-		if jx <= xmax && iy <= ymax && iy%2 !=0 && mesh_[jx,jy]!=0
-			j = mesh_[jx,jy]
-			nn_[i,2] = j
-			nn_[j,2] = i
-		end
-					
+		if BC == "PBC"
+			# z-bond
+			jx = ix - LLY 
+			jy = 1
+			if jx >= 1 && iy == ymax && mesh_[jx,jy]!=0
+				j = mesh_[jx,jy]
+				nn_[i,3] = j
+				nn_[j,3] = i
+			end
+			
+			# y-bond
+			jx = ix + 2 * LLX - 1 
+			jy = iy + 1
+			if jx <= xmax && iy <= ymax && iy%2 !=0 && mesh_[jx,jy]!=0
+				j = mesh_[jx,jy]
+				nn_[i,2] = j
+				nn_[j,2] = i
+			end
+		end			
 	
 	end
-	show(stdout, "text/plain", nn_)
-	println()
 	
-	
-	
-
-
-
-
-
-
-
-
-
+	return mesh_, nn_
 
 end
+
+mesh_, nn_ = Honeycomb(2,2,"PBC")	
+#show(stdout, "text/plain", nn_); println()
+#show(stdout, "text/plain", mesh_); println()
 
