e78dac0c1f4841cd90d64ba1a02d4da577894c73
fengshi96 <fengshichn@gmail.com>
2020-05-15 23:43:43 -0400

    update

--- main.jl
+++ main.jl
@@ -8,7 +8,7 @@ include("src/Parameter.jl")
 param = GetParameter("input.inp")
 Nstates = param.Nstates
 @time Ham = Hamiltonian(param)
-@time eigvals, eigvecs = eigs(Ham, nev = Nstates, which=:SR) # SR = Smallest Real
+@time eigvals, eigvecs = eigs(Ham, nev = Nstates, which=:SR, tol = 0.001) # SR = Smallest Real
 
 println("\nEigen values:"); show(stdout, "text/plain", real.(eigvals)); println()
 
